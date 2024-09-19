fd99187bb11eee0a8ffa42eacc4e9ee4e8b462b6
lrplrplrp <2833784318@qq.com>
2024-09-19 18:27:59 +0800

    修改模型模糊的问题

--- waifu-tips.js
+++ waifu-tips.js
@@ -31,12 +31,14 @@ async function loadWidget(config) {
 				<span class="fa fa-xl fa-times"></span>
 			</div>
 		</div>`);
-	document.getElementById("live2d").width = canvasWidth;//fa-brands fa-github   fa-solid fa-cloud 
-	document.getElementById("live2d").height = canvasHeigth;
+	document.getElementById("live2d").style.width = canvasWidth+"px";//fa-brands fa-github   fa-solid fa-cloud 
+	document.getElementById("live2d").style.height = canvasHeigth+"px";
 
+	const appWidth=canvasWidth*devicePixelRatio;
+	const appHeight=canvasHeigth*devicePixelRatio
 	const app = new PIXI.Application({
-		width: canvasWidth,
-		height: canvasHeigth,
+		width: appWidth,
+		height: appHeight,
 		view: document.getElementById('live2d'),
 		autoStart: true,
 		transparent: true
@@ -63,7 +65,15 @@ async function loadWidget(config) {
 	window.addEventListener("mousemove", () => userAction = true);
 	window.addEventListener("keydown", () => userAction = true);
 	setInterval(() => {
-		if (userAction) userActionTimer=0;
+		userActionTimer++;
+		if (userAction){
+			userActionTimer=0;
+			userAction=false;
+		} 
+		if(userActionTimer>=30){
+			loadInteraction(waifuTips.messages.idleMsg);
+			userActionTimer=0;
+		}
 	}, 1000);
 
 	let weather=await getWeather();
@@ -79,7 +89,6 @@ async function loadWidget(config) {
 				loadInteraction(interaction);
 			}
 		});
-		if(userActionTimer>=60)loadInteraction(waifuTips.messages.idleMsg);
 		const nowWeather=await getWeather();
 		if(weather!=nowWeather){
 			showWeatherText();
@@ -139,7 +148,6 @@ async function loadWidget(config) {
 					interaction
 				} of waifuTips.mouseover) {
 					if (!event.target.matches(selector)) continue;
-					console.log(interaction);
 					if(interaction)loadInteraction(interaction);
 					return;
 				}
@@ -154,7 +162,6 @@ async function loadWidget(config) {
 					return;
 				}
 			});
-			
 		});
 	}
 
@@ -177,13 +184,12 @@ async function loadWidget(config) {
 		document.querySelector("#waifu-tool .fa-github").addEventListener("click", () => {open(github);});
 		document.querySelector("#waifu-tool .fa-times").addEventListener("click", () => {
 			localStorage.setItem("waifu-display", Date.now());
-			//showMessage(waifuTips.messages.close, 11);
-			document.getElementById("waifu").style.bottom = "-500px";
+			document.getElementById("waifu").style.bottom = `-${canvasHeigth*1.5}px`;
 			setTimeout(() => {
 				//document.getElementById("waifu").style.display = "none";//有动作就会出现无头的情况
 				app.stop();
 				document.getElementById("waifu-toggle").classList.add("waifu-toggle-active");
-			}, 3000);
+			}, 1500);
 		});
 
 		// const devtools = () => { };//试了几个方法都不是很准确，放弃了
@@ -192,7 +198,6 @@ async function loadWidget(config) {
 		// };
 		// console.log("%c", devtools);
 
-		
 		window.addEventListener("copy", () => {
 			loadInteraction(waifuTips.messages.copy,null,priority=4);
 		});
@@ -237,17 +242,10 @@ async function loadWidget(config) {
 	async function getWeather() {
 		const now = new Date().getHours();
 		let weather;
-		await $.ajax({
-			type: "GET",
-			url: "https://cdn.weather.hao.360.cn/sed_api_weather_info.php?app=360chrome",
-			dataType: "jsonp",
-			jsonp: "_jsonp", //参数名  
-			success: function (result) {
-				console.log(result);
-				if (now <= 9) weather = result.weather[0].info.dawn[1];
-				else if (now > 9 && now <= 16) weather = result.weather[0].info.day[1];
-				else weather = result.weather[0].info.night[1];
-			}
+		await fetch("https://api.vore.top/api/Weather")
+			.then(response => response.json())
+			.then(result => {
+				weather=result.data.tianqi.weather;
 		});
 		return weather;
 	}
@@ -278,157 +276,6 @@ async function loadWidget(config) {
 			});
 		}
 	}
-
-	//以下是查看官方代码研究失败的废弃代码，仅供参考，从官方的代码逻辑实在是难理解参数的意思
-	/*function drawHitArea(model) {
-		const hitAreasCustom = model.internalModel.settings.json.hit_areas_custom;
-		const canvasPro=document.getElementById("live2d").getBoundingClientRect();
-		const rect=hitareaSwap(model.internalModel.originalWidth,hitAreasCustom);//internalModel.originalWidth
-		const zero={"x":(canvasPro.right-canvasPro.left)/2,"y":(canvasPro.bottom-canvasPro.top)/2}
-		console.log(rect);
-		console.log(zero);
-		let rectanglex = new PIXI.Graphics();
-		rectanglex.lineStyle(4, 0x000000, 1);
-		//rectanglex.drawRect(model.width/2, 0, 0,1000);
-		rectanglex.drawRect(model.internalModel.originalWidth/2, 0, 0,model.internalModel.originalHeight);//internalModel.originalHeight
-		rectanglex.endFill();
-		let rectangley = new PIXI.Graphics();
-		rectangley.lineStyle(4, 0x000000, 1);
-		//rectangley.drawRect(0, model.height/2, model.width,0);
-		rectangley.drawRect(0, model.internalModel.originalHeight/2, model.internalModel.originalWidth,0);
-		rectangley.endFill();
-		let rectangle = new PIXI.Graphics();
-		//rectangle.beginFill(0x66CCFF);
-		rectangle.lineStyle(4, 0xFF3300, 1);
-		//rectangle.drawRect(500, 600, 100,120);
-		//rectangle.drawRect(rect.head_x[0]+model.x, rect.head_y[1]+model.y, rect.head_y[0]-rect.head_x[0],rect.head_x[1]- rect.head_y[1]);
-		rectangle.drawRect(rect.head_x[0], rect.head_y[0], rect.head_x[1]-rect.head_x[0],rect.head_y[1]- rect.head_y[0]);
-		console.log(rect.head_x[0], rect.head_y[1], rect.head_y[0]-rect.head_x[0],rect.head_x[1]- rect.head_y[1]);
-		rectangle.endFill();
-		let rectangle1 = new PIXI.Graphics();
-		//rectangle.beginFill(0x66CCFF);
-		rectangle1.lineStyle(4, 0xFF3300, 1);
-		//rectangle1.drawRect(rect.body_x[0], rect.body_y[1], rect.body_y[0]-rect.body_x[0],rect.body_x[1]- rect.body_y[1]);
-		rectangle1.drawRect(rect.body_x[0], rect.body_y[0], rect.body_x[1]-rect.body_x[0],rect.body_y[1]- rect.body_y[0]);
-		rectangle1.endFill();
-		model.addChild(rectangle);
-		model.addChild(rectangle1);
-		model.addChild(rectanglex);
-		model.addChild(rectangley);
-		console.log(document.getElementById("live2d").getBoundingClientRect());
-		
-	}
-
-	//转换关系（鼠标x坐标-画布left）*（2/画布宽）*8/3-1
-	//（鼠标y坐标-画布top）*-（2/画布宽）*8/3+1
-	//e >= t[0] && e <= i[0] && r <= t[1] && r >= i[1]//t:head_x,i:head_y,e:转换后x,r:转换后y
-	//根据代码逻辑正常的xmin是head_x[0],xmax是head_y[0],ymin是head_y[1],ymax是head_x[1]
-	//画布中心点为0,0
-	function hitareaSwap(width,hitarea) {
-		function xSwap(width,x){
-			//return (x+1)*3/8*canvas.width/2;
-			return (x+1)*width/2;
-		}
-		function ySwap(width,y){
-			//return (y+1)*3/8*canvas.width/(-2);
-			return (y-1)*width/(-2);
-		}
-		return {
-			"head_x": [xSwap(width,hitarea.head_x[0]), xSwap(width,hitarea.head_x[1])],
-			"head_y": [ySwap(width,hitarea.head_y[0]), ySwap(width,hitarea.head_y[1])],
-			"body_x": [xSwap(width,hitarea.body_x[0]), xSwap(width,hitarea.body_x[1])],
-			"body_y": [ySwap(width,hitarea.body_y[0]), ySwap(width,hitarea.body_y[1])]
-		};
-		 var i = C.getBoundingClientRect(), //i是画布矩形
-			console.log("getBoundingClientRect", i);
-		o = $({
-				x: i.left + i.width / 2,
-				y: i.top + i.height * X
-			}, {
-				x: t.clientX, //274//0.9999999552965164
-				y: t.clientY //693//-0.7597402204070001
-			}, i),
-			n = m(o.x - i.left), //鼠标相对画布坐标
-			s = T(o.y - i.top); //
-		function h(t, i) {
-			return t.x * i.x + t.y * i.y
-		}
-
-		function l(t, i) {
-			var e = Math.sqrt(t * t + i * i);
-			return {
-				x: t / e,
-				y: i / e
-			}
-		}
-
-		function m(t) { //t[0]=2/画布宽，t[12]实在看不懂，一直等于-1
-			var i = G.transformX(t); //return this.tr[0] * t * 8 / 3 + this.tr[12] //tr[0]:缩放x tr[12]:x位置
-			return B.invertTransformX(i) //
-		}
-
-		function T(t) {
-			var i = G.transformY(t);
-			return B.invertTransformY(i)
-		}
-
-		function $(t, i, e) { //t画布中心i鼠标e画布矩形
-			function r(t, i) {
-				return 180 * Math.acos(h({
-					x: 0,
-					y: 1
-				}, l(t, i))) / Math.PI
-			}
-			if (i.x < e.left + e.width && i.y < e.top + e.height && i.x > e.left && i.y > e.top) return i;
-			var o = t.x - i.x,
-				n = t.y - i.y,
-				s = r(o, n);
-			i.x < t.x && (s = 360 - s);
-			var _ = 360 - r(e.left - t.x, -1 * (e.top - t.y)),
-				a = 360 - r(e.left - t.x, -1 * (e.top + e.height - t.y)),
-				$ = r(e.left + e.width - t.x, -1 * (e.top - t.y)),
-				u = r(e.left + e.width - t.x, -1 * (e.top + e.height - t.y)),
-				p = n / o,
-				f = {};
-			if (s < $) {
-				var c = e.top - t.y,
-					d = c / p;
-				f = {
-					y: t.y + c,
-					x: t.x + d
-				}
-			} else if (s < u) {
-				var g = e.left + e.width - t.x,
-					y = g * p;
-				f = {
-					y: t.y + y,
-					x: t.x + g
-				}
-			} else if (s < a) {
-				var m = e.top + e.height - t.y,
-					T = m / p;
-				f = {
-					y: t.y + m,
-					x: t.x + T
-				}
-			} else if (s < _) {
-				var P = t.x - e.left,
-					S = P * p;
-				f = {
-					y: t.y - S,
-					x: t.x - P
-				}
-			} else {
-				var v = e.top - t.y,
-					L = v / p;
-				f = {
-					y: t.y + v,
-					x: t.x + L
-				}
-			}
-			return f
-		} 
-	}*/
 
 	//优先级规划：交互优先级：1 ，text优先级：3，各类特殊事件优先级：9
 	function showMessage(text,priority,timeout) {
@@ -474,34 +321,34 @@ async function loadWidget(config) {
 	async function loadModel(modelId, modelTexturesId) {
 		localStorage.setItem("modelId", modelId);
 		localStorage.setItem("modelTexturesId", modelTexturesId);
-		app.stage.removeChildren();
+		//三代模型以后普遍比较大，加载时间比较久，所以隐藏一下
+		document.getElementById("waifu").style.bottom = `-${canvasHeigth*1.5}px`;
 		model = await live2d.Live2DModel.from(idSelection(modelList.models[modelId].url, modelTexturesId));
+		app.stage.removeChildren();
 		app.stage.addChild(model);
 		model.buttonMode = true;
 		autoSetTransform(modelId);
-		drawHitArea(model);
 		console.log(model);
+		drawHitArea(model);
 		console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`);
-		//setTimeout(() => {
-		document.getElementById("waifu").style.bottom = 0;
-		//}, 500);
+		document.getElementById("waifu").style.bottom = 0
 		return model;
 	}
 
 	//自动调整模型位置和大小，使其适应元素
 	function autoSetTransform(modelId) {
-		const h = canvasHeigth / model.internalModel.originalHeight;
-		const w = canvasWidth / model.internalModel.originalWidth;
+		const h = appHeight / model.internalModel.originalHeight;
+		const w = appWidth / model.internalModel.originalWidth;
 		if ('config' in modelList.models[modelId]) { //读取用户配置并修改配置
 			const config = modelList.models[modelId].config;
 			const min = Math.min(h, w)
 			model.scale.set(config.scaleX * min, config.scaleY * min);
-			model.y = (canvasHeigth - model.height) / 2 + config.y;
-			model.x = (canvasWidth - model.width) / 2 + config.x;
+			model.y = (appHeight - model.height) / 2 + config.y;
+			model.x = (appWidth - model.width) / 2 + config.x;
 		} else {
 			model.scale.set(Math.min(h, w));
-			model.y = (canvasHeigth - model.height) / 2;
-			model.x = (canvasWidth - model.width) / 2;
+			model.y = (appHeight - model.height) / 2;
+			model.x = (appWidth - model.width) / 2;
 		}
 	}
 
@@ -523,8 +370,7 @@ async function loadWidget(config) {
 
 	//设置动作
 	async function setMotion( group, index) {
-		console.log(group);
-		console.log(await model.motion(group, index),group);
+		await model.motion(group, index)
 		if (waifuTips.motion && waifuTips.motion[group]) showMessage(waifuTips.motion[group], 1);
 	}
 
@@ -570,7 +416,7 @@ async function loadWidget(config) {
 		//else showMessage("我还没有其他衣服呢！", 4000, 10);
 
 		//顺序选择
-		if (Array.isArray(modelList.models[modelId].url)) {
+		if (Array.isArray(modelList.models[modelId].url)&&modelList.models[modelId].url.length>1) {
 			const index = (++modelTexturesId >= modelList.models[modelId].url.length) ? 0 :
 				modelTexturesId;
 			loadModel(modelId, index);
@@ -578,8 +424,7 @@ async function loadWidget(config) {
 		} else loadInteraction(waifuTips.messages.notNewClothes);
 
 		//控制台输出模型材质id
-		console.log("模型ID:" + modelId);
-		console.log("材质ID:" + modelTexturesId);
+		//console.log(`Live2D 模型 ${modelId}-${modelTexturesId} 加载完成`);
 
 	}
 
