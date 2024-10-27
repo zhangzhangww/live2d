6250d263231b18c78f683a59443267e9a3bb9086
lrplrplrp <2833784318@qq.com>
2024-10-27 09:38:13 +0800

    地址修改

--- loads.js
+++ loads.js
@@ -1,6 +1,6 @@
 // 注意：live2d_path 参数应使用绝对路径
 const live2d_path = "https://fastly.jsdelivr.net/gh/lrplrplrp/live2d@master/";
-const model_path="https://raw.bgithub.xyz/lrplrplrp/live2d_api/master/";
+const model_path="https://ghproxy.net/https://raw.githubusercontent.com/lrplrplrp/live2d_api/refs/heads/master/";
 
 // 封装异步加载资源的方法
 function loadExternalResource(url, type) {
